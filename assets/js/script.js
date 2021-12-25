e5a4c97baaf0134a6fc74c4b15e29582b1cc8ff8
dimitermusic <dimiteryyordanov@gmail.com>
2021-12-25 11:15:00 -0800

    added fizzbuzz challenge solution with no help from scratch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,20 @@
+// Confirming that JavaScript file is connected to HTML file
 console.log("Hello!");
 
+// Selecting HTML elements to dynamically render content
 const ansEl = document.querySelector("#answer");
 const multiplyBtn = document.querySelector("#multiplyBtn");
 
+// Function that multiplies two arguments or paramaters
 function multiply(a, b) {
     return a * b;
 }
 
+// Confirming that multiplication function is working correctly
 console.log(multiply(2, 4));
 
+// preventDefault to prevent page from refreshing when button is clicked
+// Taking in user input from html page, plugging that into our multiplication function, and dynamically rendering the answer to the page.
 function handleAnswerDisplay(e) {
     e.preventDefault();
     const numEl1 = document.querySelector("#numEl1").value;
@@ -17,5 +23,19 @@ function handleAnswerDisplay(e) {
     ansEl.textContent = `${multiply(numEl1, numEl2)}`;
 }
 
+// Method that runs our dynamic rendering function when button is clicked.
 multiplyBtn.addEventListener("click", handleAnswerDisplay);
+
+// Fizzbuzz challenge solution from scratch with no help!
+for (let i = 1; i < 100; i++) {
+    if (i % 15 === 0) {
+        console.log("fizzbuzz");
+    } else if (i % 5 === 0) {
+        console.log("buzz");
+    } else if (i % 3 === 0) {
+        console.log("fizz");
+    } else {
+        console.log(i);
+    }
+}
 
